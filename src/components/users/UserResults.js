70dbbf09399e25b73c8d2e9e83793d43db51442b
Madeleine Woodbury <mail@woodburycode.com>
2022-01-05 11:34:42 +0100

    Display users and Spinner

---
+++ src/components/users/UserResults.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+
+const UserResults = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+
+    const data = await res.json();
+    setUsers(data);
+    setLoading(false);
+  };
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+      {users.map((user) => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export default UserResults;
